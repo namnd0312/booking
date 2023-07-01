b6f4210c5a99f5bf7ec912128359e25b25615c91
“namnd0312” <namnd0312@gmail.com>
2023-07-01 17:47:20 +0700

    init

---
+++ src/main/java/com/namnd/bookingbe/BookingBeApplication.java
@@ -0,0 +1,13 @@
+package com.namnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingBeApplication.class, args);
+    }
+
+}
