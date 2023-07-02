b30dbefd65d2798d98860bf09595265b6563065f
“namnd0312” <namnd0312@gmail.com>
2023-07-03 00:59:29 +0700

    add redis config

--- src/main/java/com/namnd/bookingbe/config/redis/RedisConfig.java
+++ src/main/java/com/namnd/bookingbe/config/redis/RedisConfig.java
@@ -1,34 +1,39 @@
-//package com.namnd.bookingbe.config.redis;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//
-//@Configuration
-//public class RedisConfig {
-//
-//    @Value("${spring.redis.host}")
-//    private String redisHost;
-//
-//    @Value("${spring.redis.port}")
-//    private int redisPort;
-//
-//    @Bean
-//    public JedisConnectionFactory jedisConnectionFactory() {
-//        JedisConnectionFactory factory = new JedisConnectionFactory();
-//        factory.setHostName(this.redisHost);
-//        factory.setPort(this.redisPort);
-//        factory.setTimeout(600000);
-//        factory.getPoolConfig().setMaxIdle(100);
-//        return factory;
-//    }
-//
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate() {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        template.setConnectionFactory(jedisConnectionFactory());
-//        return template;
-//    }
-//}
+package com.namnd.bookingbe.config.redis;
+
+
+import com.namnd.bookingbe.config.ApplicationProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+
+@Configuration
+@ConditionalOnProperty(value = "application.redis.enable", havingValue = "true")
+public class RedisConfig {
+
+
+    private final ApplicationProperties applicationProperties;
+
+    public RedisConfig(ApplicationProperties applicationProperties) {
+        this.applicationProperties = applicationProperties;
+    }
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName(applicationProperties.getRedis().getHost());
+        factory.setPort(applicationProperties.getRedis().getPort());
+        factory.setTimeout(applicationProperties.getRedis().getTimeout());
+        factory.getPoolConfig().setMaxIdle(applicationProperties.getRedis().getMaxTotal());
+        return factory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        return template;
+    }
+}
